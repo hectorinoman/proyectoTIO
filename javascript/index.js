542659a9e5f0df4cced5d93feb5464e02f2c72e7
rebecca <rebe.mart.gal@gmail.com>
2015-12-14 15:42:17 +0000

    probando php

--- javascript/index.js
+++ javascript/index.js
@@ -1,11 +1,11 @@
-var btnPostact, direccion, btnBuscar, geocoder;
+var btnPostact, direccion, btnBuscar, geocoder, tipo, x;
 
 //funcion que inicialia las variables y los EventListeners
 function initialize() {
   btnPosact = document.getElementById('btn_posact');
   btnBuscar = document.getElementById('btn_buscar');
   geocoder =  new google.maps.Geocoder();
-
+  tipo = document.getElementById('tipo').value;
   //EventListeners
   btnBuscar.addEventListener("click", buscar);
   btnPosact.addEventListener("click", localizacion);
@@ -13,6 +13,7 @@ function initialize() {
 
 window.onload = initialize;
 
+//Si la direccion esta vacía muestra un error, sino calcula la latitud y la longitud de la dirección introducida.
 function buscar() {
   var direccion = document.getElementById('lugar').value;
   if(direccion == ""){
@@ -20,11 +21,16 @@ function buscar() {
   }
   else {
     geocoder.geocode( { 'address': direccion }, function(results, status) {
+      //Obtenemos la latitud y longitud de la dirección obtenida
       if (status == google.maps.GeocoderStatus.OK) {
-        var latitude = results[0].geometry.location.lat();
-        var longitude = results[0].geometry.location.lng();
-        alert(latitude);
-        console.log(longitude);
+        var latitud = results[0].geometry.location.lat();
+        var longitud = results[0].geometry.location.lng();
+        //Enviamos la latitud y la longitud al php
+        $.post('bucar.php', { latitud: latitud, longitud: longitud, tipo: tipo });
+        $.get("../php/bucar.php", function(data) {
+          x = data;
+          console.log(x);
+        });
       }
     });
   }
@@ -38,10 +44,18 @@ function localizacion(){
   }
 }
 
+//Calcula la posicion actual del usuario.
 function exito(position){
   var latitud = position.coords.latitude;
   var longitud = position.coords.longitude;
+  //Enviamos la latitud y la longitud al php
+  $.post('../php/bucar.php', { latitud: latitud, longitud: longitud, tipo: tipo });
+  $.get("../php/bucar.php", function(data) {
+      x = data;
+      console.log(x);
+  });
 }
+
 function fracaso(){
   $('#error_buscar').append("<p class='error_p'>No se pudo geolocalizar su posición<p>");
 }
---
+++ php/buscar.php
@@ -0,0 +1,32 @@
+<?php
+$servername = "localhost";
+$username = "alu4570";
+$password = "7Nd1JO";
+$dbname = "alu4570";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$conn->close();
+
+//Conectando, seleccionando la base de datos
+$link = mysql_connect($servername,$username,$password)
+    or die('No se pudo conectar: ' . mysql_error());
+mysql_select_db($dbname) or die('No se pudo seleccionar la base de datos');
+
+$latitud = $_POST['latitud'];
+$longitud = $_POST['longitud'];
+$tipo = $_POST['tipo'];
+$query = "SELECT * FROM '$tipo' WHERE NOMBRE = 'Campo de Barranco Grande' ";
+$result = mysql_query($query);
+if(mysql_num_rows($result) > 0) {
+        echo "bieen";
+}
+else{
+        echo "maaal";
+}
+// Liberar resultados
+mysql_free_result($result);
+?>
